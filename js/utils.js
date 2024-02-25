92f26e84a185b6e1b7a0413f645e685d7b321307
limys7x7x7 <capysta@list.ru>
2024-02-25 14:22:35 +0300

    777

--- js/utils.js
+++ js/utils.js
@@ -7,25 +7,27 @@ export function getTasksLocalStorage() {
 export function setTasksLocalStorage(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks))
 }
-// Функция генерирует ID для созданных элементов списка ToDo
+Функция генерирует ID для созданных элементов списка ToDo
 export function generateId() {
   const times = Date.now()
   const randomPart = Math.floor(Math.random() * 10_000)
-  return times + randomPart
+  return times
 }
+// Функция обновляет содержимое списка удаляет и перерендерит из localStorage
 export function updateListTasks() {
   document.querySelector('.list').textContent = ''
   const todosArray = getTasksLocalStorage()
   renderTasks(todosArray)
 }
 function renderTasks(tasksArray) {
+  // Если массива нет или длинна массива 0 то не рендерим. Потому что нечего!!
   if (!tasksArray || !tasksArray.length) return
-
+// Пробегаемся циклом по обьектам массива и вставляем их в список.
   tasksArray.forEach((value) => {
     const { id, text } = value   // Деструкторизация 
     const item = `<div class="task" data-task-id="${id}">
     <li class="list__li">
-    <strong class="task__text">${text}</strong>
+    <p class="task__text">${text}</p>
     <div class="list__btns">
     <button class="button list__buttonEdit">Ред</button>
     <button class="button list__buttonDelete">Удалить</button></div>
